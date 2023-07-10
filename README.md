74353ba491c1add29fc17975beba391124722e1e
vijay <Vijay.PV@expeditors.com>
2023-07-10 15:02:28 +0530

    added PostAuthorize PreFilter and PostFilter

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # spring-security-6
 Spring Security 6.0 using spring boot 3
 
-### Running in different types of security
+### Running in different Configurations of security
 ->For no security set security-no-security to true and others to false
 <br>
 ->For basic security set security-in-memory to true and others to false
--- src/main/java/com/hacked/springsecurity6/Controller/DemoController.java
+++ src/main/java/com/hacked/springsecurity6/Controller/DemoController.java
@@ -1,17 +1,26 @@
 package com.hacked.springsecurity6.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @RestController()
 public class DemoController {
 
+    Logger logger = LoggerFactory.getLogger(DemoController.class);
     @GetMapping("/demo")
     @PreAuthorize("hasAuthority('read')")
     public String demo() {
@@ -50,4 +59,26 @@ public class DemoController {
         return "demo4";
     }
 
+//    @PostAuthorize
+    @GetMapping("/demo5")
+    @PostAuthorize("hasAuthority('match')")
+    public String demo5() {
+        logger.info("Post Authorize method called");
+        return "Demo5: Post Authorize";
+    }
+
+//    @PreFilter => works best with either array or Collection
+    @GetMapping("demo6")
+    @PreFilter("filterObject.contains('a')")
+    public String demo6(@RequestBody List<String> values)   {
+        return "Demo: PreFilter";
+    }
+
+//    @PostFilter => not so recommended as it doesn't find much use case
+//    @PostFilter filter the return value unlike @PreFilter which filters the incoming input value
+    @GetMapping("demo7")
+    @PostFilter("filterObject.contains('a')")
+    public List<String> demo7() {
+        return  new ArrayList<>(Arrays.asList("asd", "aswe","dfghj","awwq"));
+    }
 }
